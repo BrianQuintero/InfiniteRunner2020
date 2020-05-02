5958d1a8210fdf321f6ef5e396e4a645ec86e8eb
BrianQuintero <bquinte1@ucsc.edu>
2020-05-01 21:44:24 -0700

    Game prototype version 1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
+//[Game Name], a project by: Jeven Zarate McCoy, Giovanni Lua-Trejo, Brian Quintero
+//Date Completed: 
+//We decided to get creative with the idea of playing Golf somewhere on the moon
 let config ={
     type: Phaser.CANVAS,
     width: 640,
-    height: 480
+    height: 480,
+    physics: {
+        default: 'arcade',
+        arcade:{
+            debug: false,
+            gravity:{
+                x:0,
+                y:0
+            }
+        }
+    },
+    scene: [Menu, Play]
 }
+
 let game = new Phaser.Game(config);
+let keyRIGHT, keyLEFT, keyUP, keyDOWN, keyR;
---
+++ src/scenes/Menu.js
@@ -0,0 +1,12 @@
+class Menu extends Phaser.Scene{
+    constructor(){
+        super("mainMenu");
+    }
+    create(){
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+        keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+        this.scene.start("playScene");
+    }
+}
